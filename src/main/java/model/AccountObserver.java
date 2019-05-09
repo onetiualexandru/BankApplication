41fb29e5f741ad24752b82499aec0d5540d77fdb
Onetiu Alexandru <onetiualex@yahoo.com>
2019-05-09 08:19:29 +0300

    initial commit

--- src/main/java/model/AccountObserver.java
+++ src/main/java/model/AccountObserver.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 public class AccountObserver {
 
     ArrayList<Account> obs=new ArrayList<Account>();
+    Account account=new Account();
 
     public AccountObserver(Account account){
-
-    }
-    public AccountObserver( ArrayList<Account> obs){
-        this.obs=obs;
+        this.account=account;
     }
 
     public String update(Account account) {
